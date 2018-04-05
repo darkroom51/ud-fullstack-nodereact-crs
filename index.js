c62eb97504e3969471b732d661afe9b6451830ab
darkroom51 <darkroom@wp.pl>
2018-04-05 03:01:25 +0200

    oauth, cookie, logout

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');  // importing whole file, creating model class
+require('./services/passport'); // importing whole file
+
+
+mongoose.connect(keys.mongoURI); // connect to mongodb
 
 const app = express();
-require('./routes/authRoutes')(app);
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app); // importing function
 
 
 
--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,41 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users'); // pull out User from model class
+
+passport.serializeUser((user, done) => { // set cookie
+	done(null, user.id); // this id is record _id shortcut
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+		.then(user => done(null, user))
+		.catch(err => console.log('deserialize user: ', err));
+});
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-      },
-      (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-      }
-    )
-  );
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			// console.log(accessToken, profile);
+			User.findOne({ googleId: profile.id })
+				.then((existingUser) => {
+					if (existingUser) {
+						//we already have a record
+						done(null, existingUser);
+					} else {
+						new User({ googleId: profile.id }) // creating model instance
+							.save() // saving to db
+							.then(user => done(null, user)) // finishing process with new instace which should be further use
+							.catch(err => console.log('save() new user:', err));
+						}
+				})
+		}
+	)
+);
